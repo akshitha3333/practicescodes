8491d3072a2f2918071895f7f9b119aade4bfe34
Akshitha <akshitha@example.com>
2026-01-25 17:36:27 +0530

    forms

--- ws/fto.js
+++ ws/fto.js
@@ -136,24 +136,60 @@ const { default: axios } = require("axios")
     })
  }
  //sendotp()
-
- let baseurl1 = "https://online-exam-backend-production-12ba.up.railway.app"
- function verifyotp(){
-    let url = baseurl1 + "/api/auth/verify-otp"
+function verifyotp() {
+    let url = baseurl + "/api/auth/verify-otp";
+    
     let body = {
-        authLogId: "fa0a2cc6-4860-442b-ac7f-97aa9676ac4c",
-        otp: "231610"
-    }
-    axios(url,body,{
-        headers : {
-            "content-type" : "application/json"
+        authLogId: "49f0518f-bb1b-4d2e-9a19-6f6142ba5214",
+        otp: "403646"
+    };
+
+    axios.post(url, body, {
+        headers: {
+            "Content-Type": "application/json"
         }
-    }).then(response=>{
-        console.log(response.data);
-    }).catch(error=>{
-        console.log(error.message);
-        console.log(error.name);
     })
- }
- verifyotp()
- 
+    .then(response => {
+        console.log(response.data);
+    })
+    .catch(error => {
+        console.log("Status:", error.response?.status);
+        console.log("Message:", error.response?.data || error.message);
+    });
+}
+
+verifyotp();
+function addstates(){
+    let data={
+  "stateName": "madhyapradesh",
+  "country": "India"
+}
+let url=baseurl + "api/geo/states"
+axios.post(url,data,{
+    headers:{
+    "content-type":"application/json",
+    "authorization":"Bearer eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiQURNSU5JU1RSQVRPUiIsInBlcm1pc3Npb25zIjpbXSwic3ViIjoiZDIxNGUzZDgtN2I0Mi00MGFjLWE0YzMtOWIzYTk1Nzg2ZDM4IiwiaWF0IjoxNzY5MTUxMDQzLCJleHAiOjE3NjkyMTI3OTl9.CHuC3NUzxEN4fLoULI8BlMMpWy_svN5c23rtlxJPFPU"
+    }
+}).then(response=>{
+    console.log(response.data);
+}).catch(err=>{
+    console.log(err);  
+})
+}
+//addstates()
+function getstates(){
+    let url=baseurl+"/api/geo/states"
+    axios.get(url,{
+        headers:{
+        "content-type":"application/json",
+        "authorization":"Bearer eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiQURNSU5JU1RSQVRPUiIsInBlcm1pc3Npb25zIjpbXSwic3ViIjoiZDIxNGUzZDgtN2I0Mi00MGFjLWE0YzMtOWIzYTk1Nzg2ZDM4IiwiaWF0IjoxNzY5MTUxMDQzLCJleHAiOjE3NjkyMTI3OTl9.CHuC3NUzxEN4fLoULI8BlMMpWy_svN5c23rtlxJPFPU"
+    }
+}).then(response=>{
+        console.log(response.data);
+        
+    }).catch(err=>{
+        console.log(err);
+        
+    })
+}
+getstates()
